3aca2755eae2889cf3d4d10c84b39c4f9c91e694
Susana vik <susana.m.vik@gmail.com>
2021-02-12 00:16:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.collect {|element| element = valid_calls}
+  planeteer_calls.collect {|element| valid_calls = element}
   binding.pry
 end
 
